bd82436dd84455d5a888bb307425a74b7b9d4bfe
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-03-06 18:02:46 +0200

    create class Database
    create class Database and changed all the functions to methods

--- app.js
+++ app.js
@@ -1,42 +1,30 @@
 require("dotenv").config();
+const fs = require("fs");
 const express = require("express");
 const cors = require("cors");
+const { captureRejectionSymbol } = require("events");
 const app = express();
-const fs = require("fs");
-
+const Database = require("./database/database.js");
+const database = new Database();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
-
 app.use("/public", express.static(`./public`));
-// app.use("/", express.static(`./views`)); //??
+// app.use("/", express.static(`./views`));
 
 app.post("/api/shorturl/new", async (req, res) => {
   const fullUrl = req.body.url;
-  if (!fullUrl) {
-    return res.status(404).json({ message: "url not exist" });
-  } else if (!isUrl(fullUrl)) {
-    return res.status(422).json({ message: "Not a valid url" });
+  if (isUrl(fullUrl)) {
+    if (database.checkIfExists(fullUrl) === -1) {
+      database.addNewLink(fullUrl);
+      database.saveDatabase();
+      res.json(database.createLinkObj(fullUrl, true));
+    } else {
+      res.json(database.getObjById(fullUrl));
+    }
+  } else {
+    res.json({ error: "URL is not valid!" });
   }
-  let jsonContent = JSON.stringify(req.body);
-  fs.writeFile(`./database/DB.json`, jsonContent, "utf8", function (err) {
-    if (err) {
-      console.log("An error occur while writing JSON Object to File.");
-      return console.log(err);
-    }
-    console.log("JSON file has been saved.");
-  });
-  res.send(req.body);
-});
-
-app.get("/api/shorturl/new", (req, res) => {
-  fs.readFile("./database/DB.json", "utf8", (err, data) => {
-    if (err) {
-      res.status(404).json({ message: "Not found!" });
-      return;
-    }
-    res.send(data);
-  });
 });
 
 function isUrl(text) {
@@ -54,3 +42,5 @@ app.get("/", (req, res) => {
 });
 
 module.exports = app;
+
+function containURL(urlToCheck) {}
--- database/database.js
+++ database/database.js
@@ -1,29 +1,81 @@
-class Database{
-  constructor() {}
+const fs = require("fs");
+let database;
 
-//  writeURL(url) { }
+class Database {
+  constructor() {
+    this.initDB = this.initDB();
+  }
 
-  checkExists(fullUrl) {
-   const database = `./database/DB.json`
-  if (database.checkExists({ fullUrl }.json)) {
-    return true;
-  } else {
-    return false;
+  initDB() {
+    this.getData();
+  }
+  writeData(content) {
+    fs.writeFile("./database/DB.json", content, "utf8", function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("Data was saved!");
+    });
+  }
+
+  getData() {
+    fs.readFile("./database/DB.json", "utf8", function (err, data) {
+      if (err) {
+        throw err;
+      }
+      database = JSON.parse(data);
+    });
+  }
+
+  checkIfExists(url) {
+    for (let i in database) {
+      if (database[i].fullUrl === url) {
+        return database[i].shorturl;
+      }
+    }
+    return -1;
+  }
+
+  getNewId() {
+    return database[0].idcount + 1;
+  }
+
+  addNewLink(url) {
+    database.push(this.createLinkObj(url));
+    database[0].idcount = this.getNewId();
+  }
+
+  createLinkObj(url, flag) {
+    let obj;
+    if (!flag) {
+      obj = { shorturl: this.getNewId(), fullUrl: url };
+    } else {
+      obj = { shorturl: this.getIdByLink(url), fullUrl: url };
+    }
+    return obj;
+  }
+
+  getIdByLink(url) {
+    for (let i in database) {
+      if (database[i].fullUrl === url) {
+        return database[i].shorturl;
+      }
+    }
+    return -1;
+  }
+
+  saveDatabase() {
+    this.writeData(JSON.stringify(database));
+  }
+
+  getObjById(url) {
+    for (let i in database) {
+      if (database[i].fullUrl === url) {
+        return database[i];
+      }
+    }
+    return -1;
   }
 }
 
- isExists((req, res) => {
-    if (fullUrl === true) {
-        return res.shortUrl;
-    } else {
-      createShortUrl();
-    }
-});
-
- getUrl(url) {}
-
-function createShortUrl(fullUrl) {
-
- }
-
-}
+module.exports = Database;
